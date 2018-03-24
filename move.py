fd37b5e09b8b45d2aa69f7e44ce26ef3b9b66855
Marron Akanishi <marron.general@gmail.com>
2018-03-24 15:42:32 +0900

    リファクタリング

--- move.py
+++ move.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import shutil
 import json
 
@@ -10,16 +9,13 @@ filelist = json.load(jsonfile)
 jsonfile.close()
 study_list = {}
 test_list = {}
-count = 0
 
-for filepath in filelist.keys():
-    study = True
+for count, filepath in enumerate(filelist.keys()):
     _, filename = os.path.split(filepath)
     # 10%をテスト用にする
     if count % 10 == 1:
         movedir = folder+"test/"+str(filelist[filepath])+"/"
         test_list[movedir+filename] = filelist[filepath]
-        study = False
     else:
         movedir = folder+"study/"+str(filelist[filepath])+"/"
         study_list[movedir+filename] = filelist[filepath]
@@ -28,7 +24,6 @@ for filepath in filelist.keys():
         os.mkdir(movedir)
     # ファイルコピー
     shutil.copyfile(filepath, movedir+filename)
-    count += 1
 
 jsonfile = open("./study.json","w")
 json.dump(study_list, jsonfile, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
